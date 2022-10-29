9c90cae3d5bd324a8d7d079ee78ee507a26ef1fd
Lucas de Oliveira Gonçalves <lucas.oliver_2000@hotmail.com>
2022-10-28 22:40:08 -0300

    Add project files.

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Orientação a Objetos".ToUrl());
+
+//Classe estática que possui funções de extensões para o tipo String. Por ser estática e possuir o "this" no parâmetro, poderia ser acessada de qualquer lugar do projeto
+public static class StringExtension
+{
+    public static string ToUrl(this string texto) => Regex.Replace(texto, @"\s+", "-").ToLower();
+}
